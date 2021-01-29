c7939de6e2208dc7499c94a269e88e35a4619642
pn88 <piotrnoga88@gmail.com>
2021-01-29 20:50:20 +0100

    add some data

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SpeedConverter speedConverter = new SpeedConverter();
-        System.out.println(speedConverter.toMilesPerHour(16));
-    }
-}
