0ecf86cc14adb9023e592c45e4bc6cf66ea9cd89
pn88 <piotrnoga88@gmail.com>
2021-01-29 20:47:23 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SpeedConverter speedConverter = new SpeedConverter();
+        System.out.println(speedConverter.toMilesPerHour(16));
+    }
+}
