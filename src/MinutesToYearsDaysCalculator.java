959af2c5efb179e8d5138738cd1ec8ba3669290f
pn88 <piotrnoga88@gmail.com>
2021-01-30 13:55:43 +0100

    add

---
+++ src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,14 @@
+public class MinutesToYearsDaysCalculator {
+    public static void printYearsAndDays(long minutes){
+        if (minutes<0){
+            System.out.println("Invalid Value");
+        }else
+        {
+            int year= (int)minutes/525600;
+            int remainder=(int)minutes%525600;
+            int days=remainder/1440;
+
+            System.out.println(minutes+" min = "+year+" y and "+days+" d");
+        }
+    }
+}
