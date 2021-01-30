3f28fd7bafbda4a88ae827614b82b24a9d40af0d
pn88 <piotrnoga88@gmail.com>
2021-01-30 21:06:55 +0100

    add new classes

---
+++ src/com/pinodev/animal/Dog.java
@@ -0,0 +1,46 @@
+package com.pinodev.animal;
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+    private void chew(){
+        System.out.println("Dog.chew() ");
+    }
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat");
+        chew();
+        super.eat();
+    }
+    public void walk(){
+        System.out.println("Dog.walk");
+        move(5);
+    }
+    public void run(){
+        System.out.println("Dog.run");
+        move(10);
+    }
+    public void moveLegs(int speed){
+        System.out.println("Dog.moveLegs()");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move");
+        moveLegs(speed);
+        super.move(speed);
+    }
+}
