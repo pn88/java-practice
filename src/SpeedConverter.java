7ed91051e647deec143583062ae14ce067b2dda1
pn88 <piotrnoga88@gmail.com>
2021-01-30 13:40:21 +0100

    add MB Converter

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -2,7 +2,7 @@ public class SpeedConverter {
 
     public long toMilesPerHour(double kilometersPerHour) {
 
-        if(kilometersPerHour < 0) {
+        if (kilometersPerHour < 0) {
             return -1;
         }
 
@@ -11,7 +11,7 @@ public class SpeedConverter {
 
     public void printConversion(double kilometersPerHour) {
 
-        if(kilometersPerHour <0) {
+        if (kilometersPerHour < 0) {
             System.out.println("Invalid Value");
         } else {
             long milesPerHour = toMilesPerHour(kilometersPerHour);
