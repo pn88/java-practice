17b70deb519153bf80bb1104c633a1fcd3739fe3
pn88 <piotrnoga88@gmail.com>
2021-01-30 15:20:25 +0100

    add some data

---
+++ src/LargestPrime.java
@@ -0,0 +1,25 @@
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+        int n = number;
+        int n1 = n;
+        int count = 0;
+        while (n != 0) {
+            if ((number % n) == 0) {
+                for(n1=n;n1!=0;n1--){
+                    if((n % n1) == 0){
+                        count++;
+                    }
+                }
+                if(count == 2){
+                    return n;
+                }
+                count-=count;
+            }
+            n--;
+        }
+        return -1;
+    }
+}
