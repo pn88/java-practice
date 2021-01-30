5dde2c76c34bff666843869314fdecf7426e4b02
pn88 <piotrnoga88@gmail.com>
2021-01-30 13:41:42 +0100

    add MB Converter

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,16 @@
+    public class MegaBytesConverter {
+
+        public static void printMegaBytesAndKiloBytes(int kiloBytes){
+            // 1 MB = 1024kb
+
+            int megaBytes;
+            int extraKiloBytes;
+            megaBytes =  kiloBytes/1024;
+            extraKiloBytes =  kiloBytes%1024;
+            if(kiloBytes <0){
+                System.out.println("Invalid Value");
+            }else {
+                System.out.println(kiloBytes+" KB = "+megaBytes+" MB and " + extraKiloBytes +" KB");
+            }
+        }
+    }
