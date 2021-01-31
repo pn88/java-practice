f6768bcb1cfd6e230f4c34372697fbf308d5a3a1
pn88 <piotrnoga88@gmail.com>
2021-01-31 20:25:48 +0100

    add array

---
+++ src/com/pinodev/basics/Array.java
@@ -0,0 +1,28 @@
+package com.pinodev.basics;
+
+import java.util.Scanner;
+
+public class Array {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element "+ i + ", typed value was "+myIntegers[i]);
+        }
+    }
+    public static int[] getIntegers(int number){
+        System.out.println("Enter "+ number + " integer values \r");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++) {
+            values[i]= scanner.nextInt();
+        }
+        return values;
+    }
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum+=array[i];
+        }
+        return (double) sum/ (double)array.length;
+    }
+}
