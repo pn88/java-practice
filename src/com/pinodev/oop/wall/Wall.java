838a11eb27984511d290d1f8286843ffdbe627aa
pn88 <piotrnoga88@gmail.com>
2021-02-06 11:32:41 +0100

    add data

---
+++ src/com/pinodev/oop/wall/Wall.java
@@ -0,0 +1,53 @@
+package com.pinodev.oop.wall;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea() {
+        double area = width * height;
+        System.out.println("The area is " + area);
+        return area;
+    }
+
+    public Wall(double width, double height) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public Wall() {
+        this(0, 0);
+    }
+}
